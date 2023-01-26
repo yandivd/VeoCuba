81dd51af2198894b58f1c2978e5b3660768bdfb6
yandivd <yandivd@gmail.com>
2023-01-26 17:36:47 -0500

    validacion de imagenes

--- main/forms.py
+++ main/forms.py
@@ -8,14 +8,14 @@ class ImagenForm(forms.ModelForm):
         fields = 'nombre','ig','categoria','provincia','municipio','referencia','email','telf','direccion','foto'
     
     #metodo para validar imagenes not working yet
-    def clean_image(self):
-        image = self.cleaned_data['foto']
-        if image:
-            if image.__size < 4*1024*1024:
-                raise ValidationError("Imagen demasiado grande")
-            return image
-        else:
-            raise ValidationError("No se cargo la imagen")
+    # def clean_image(self):
+    #     image = self.cleaned_data['foto']
+    #     if image:
+    #         if image.__size < 4*1024*1024:
+    #             raise ValidationError("Imagen demasiado grande")
+    #         return image
+    #     else:
+    #         raise ValidationError("No se cargo la imagen")
 
 class ContactoForm(forms.ModelForm):
 
